decfaa03ef75382bee26bd163ced4ea04f8f71d2
vinaykashyap1996 <vinay.kshyap234@gmail.com>
2019-11-10 14:46:04 +0100

    index.js

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ module.exports = app;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const async=require('async');
+const async = require("async");
+const port = process.env.PORT || 3002;
 
 const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
 require("./config/passport")(passport);
 
-mongoose.connect(process.env.db_url, {useNewUrlParser: true}, function (err) {
-    if (err) console.log('Error While connecting to DB:',err);
-    else console.log("DB Connected Successfully");
+mongoose.connect(process.env.db_url, { useNewUrlParser: true }, function(err) {
+  if (err) console.log("Error While connecting to DB:", err);
+  else console.log("DB Connected Successfully");
 });
-  
-  app.use(cors());
-  global.mongoose=mongoose;
-  global.async = async;
-  app.use(bodyParser.json({ limit: "50mb" }));
-  app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get("/", (req, res) => {
-    res.send("NodeJS");
-  });
-  const user=require('./routes/user');
-  app.use("/user",user);
+app.use(cors());
+global.mongoose = mongoose;
+global.async = async;
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  const news=require('./routes/news')
-  app.use('/news',news)
+app.get("/", (req, res) => {
+  res.send("NodeJS");
+});
+const user = require("./routes/user");
+app.use("/user", user);
 
-  const rating=require('./routes/ratings')
-  app.use('/rating',rating)
+const news = require("./routes/news");
+app.use("/news", news);
 
-  app.listen(3002, () => {
-    console.log("server running on port number 3002");
-  })
+const rating = require("./routes/ratings");
+app.use("/rating", rating);
+
+app.listen(port, () => {
+  console.log("server running on port number 3002");
+});
